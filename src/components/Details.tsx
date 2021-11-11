e4e30b50f752a59b96152d249d1013c62a3e9c35
sea-lakhan <lakhansagar2695@gmail.com>
2021-11-11 18:45:51 +0530

    chages for redux task

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,31 +1,37 @@
-import React from 'react'
-import Button from '@material-ui/core/Button';
-import {  useNavigate } from 'react-router';
-import { initialState, selectUser } from '../redux_stuff/userSlicer';
-import { useDispatch, useSelector } from 'react-redux';
-import { removeUser } from '../redux_stuff/userSlicer';
-export const Details = (props:any) => {
-    const user=useSelector(selectUser);
-    const dispatch=useDispatch();
-    const navigation=useNavigate();
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { useNavigate } from "react-router";
+import { selectUser } from "../redux_stuff/userSlicer";
+import { useDispatch, useSelector } from "react-redux";
+import { removeUser } from "../redux_stuff/userSlicer";
+import { makeStyles } from "@material-ui/core";
 
+const useStyle = makeStyles({
+  heading: { display: "flex", color: "#898989", marginBottom: 5 },
+  button: { width: "100%", marginTop: 5, height: "3rem" },
+});
 
-    const handleClick=async()=>{
-        await dispatch(removeUser(initialState))
-        navigation('..')
-    }
+export const Details = (props: any) => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const navigation = useNavigate();
+  const styles = useStyle();
+  const handleClick = () => {
+    dispatch(removeUser());
+    navigation("..");
+  };
 
-    return (
-        <div className="signupContainer">
-        <div className="detailsContainer">
-            <h2 style={{display:"flex",color:"#898989", marginBottom:5}}>User Details</h2>
-            <h3>Name:{user.name}</h3>
-            <h3>Email:{user.email}</h3>
-            <h3>Username:{user.username}</h3>
-            <Button onClick={handleClick} style={{ width:"100%", marginTop:5,height:"3rem"}} variant="contained" color="primary">
-                Back
-            </Button>
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className="signupContainer">
+      <div className="detailsContainer">
+        <h2 className={styles.heading}>User Details</h2>
+        <h3>Name:{user.name}</h3>
+        <h3>Email:{user.email}</h3>
+        <h3>Username:{user.username}</h3>
+        <Button className={styles.button} onClick={handleClick} variant="contained" color="primary">
+          Back
+        </Button>
+      </div>
+    </div>
+  );
+};
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,64 +1,106 @@
-import React, { ChangeEvent, useEffect, useState } from 'react'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { useDispatch } from 'react-redux';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { useDispatch } from "react-redux";
+import { makeStyles } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
 
-import { useNavigate } from 'react-router-dom';
+import { addUser, initialState } from "../redux_stuff/userSlicer";
 
-import {addUser} from '../redux_stuff/userSlicer';
+const useStyle = makeStyles({
+  heading: { display: "flex", color: "#898989" },
+  textField: { width: "100%", backgroundColor: "#ffffff", borderRadius: 10, marginTop: 10 },
+  credentialContainer: { display: "flex", width: "100%", justifyContent: "space-between", alignItems: "center" },
+  credentialTextFields: { width: "49%", backgroundColor: "#ffffff", borderRadius: 10, marginTop: 10 },
+  button: { width: "100%", marginTop: 5, height: "3rem" },
+});
+
 export const Signup = () => {
-    const dispatch=useDispatch();
-    const navigation=useNavigate();
- 
-    const[name, setName]=useState('');
-    const[email, setEmail]=useState('');
-    const[username, setUsername]=useState('');
-    const[password, setPassword]=useState('');
+  const dispatch = useDispatch();
+  const navigation = useNavigate();
+  const [user, setUser] = useState(initialState);
+  const styles = useStyle();
+  // const[name, setName]=useState('');
+  // const[email, setEmail]=useState('');
+  // const[username, setUsername]=useState('');
+  // const[password, setPassword]=useState('');
 
-    const handleChange=(e:ChangeEvent<HTMLInputElement>, type:string)=>{
-        if(type==="name")
-            setName(e.target.value)
-        else if(type==='email')
-            setEmail(e.target.value)
-        else if(type==='username')
-            setUsername(e.target.value)
-        else if(type==='password')
-            setPassword(e.target.value)
+  const handleChange = (value: string, type: string) => {
+    switch (type) {
+      case "name":
+        // setName(value)
+        setUser({ ...user, name: value });
+        break;
+      case "email":
+        // setEmail(value)
+        setUser({ ...user, email: value });
+        break;
+      case "username":
+        // setUsername(value)
+        setUser({ ...user, username: value });
+        break;
+      case "password":
+        // setPassword(value)
+        setUser({ ...user, password: value });
+        break;
     }
+  };
 
-    useEffect(() => {    
-        setPassword('');
-    }, [username])
+  useEffect(() => {
+    setUser({ ...user, password: "" });
+  }, [user.username]);
 
-    const handleClick=async ()=>{     
-        if(name && email && username && password){
-            
-        console.log(name,email,username,password);
-            await dispatch(addUser({name:name,email:email,username:username,password:password}))
-            navigation('/details')
-        }
-        else {
-            alert("Please enter all the details");
-        }
-        
+  const handleClick = () => {
+    if (user.name && user.email && user.username && user.password) {
+      dispatch(addUser(user));
+      navigation("/details");
+    } else {
+      alert("Please enter all the details");
     }
+  };
 
-    return (
-        <div className="signupContainer">
-            <div className="formfields">
-                <h1 style={{display:"flex",color:"#898989"}}>Sign Up</h1>
-                <TextField style={{width:"100%", backgroundColor:'#ffffff', borderRadius:10, marginTop:10}} onChange={(event:ChangeEvent<HTMLInputElement>)=>handleChange(event,'name')} id="outlined-basic" label="Name" variant="outlined" />
-                <TextField style={{width:"100%",backgroundColor:'#ffffff', borderRadius:10, marginTop:10}} onChange={(event:ChangeEvent<HTMLInputElement>)=>handleChange(event,'email')} id="outlined-basic" label="Email" variant="outlined" />
-                <div style={{display:'flex',width:"100%", justifyContent:"space-between", alignItems:'center'}}>
-                <TextField style={{width:"50%", backgroundColor:'#ffffff', borderRadius:10, marginTop:10, marginRight:5}} onChange={(event:ChangeEvent<HTMLInputElement>)=>handleChange(event,'username')} id="outlined-basic" label="Username" variant="outlined" />
-                <TextField type="password" value={password} style={{width:"50%", backgroundColor:'#ffffff', borderRadius:10, marginTop:10, alignSelf:"flex-end"}} onChange={(event:ChangeEvent<HTMLInputElement>)=>handleChange(event,'password')} id="outlined-basic" label="Password" variant="outlined" />
-                </div>
-                {/* <Link style={{ width:"100%", marginTop:10,height:"3rem", font}} to={{pathname:'/details'}}> */}
-                <Button onClick={handleClick} style={{ width:"100%", marginTop:5,height:"3rem"}} variant="contained" color="primary">
-                    Submit
-                </Button>
-                {/* </Link> */}
-            </div>
+  return (
+    <div className="signupContainer">
+      <div className="formfields">
+        <h1 className={styles.heading}>Sign Up</h1>
+        <TextField
+          className={styles.textField}
+          onChange={(e) => handleChange(e.target.value, "name")}
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+        />
+        <TextField
+          className={styles.textField}
+          onChange={(e) => handleChange(e.target.value, "email")}
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+        />
+        <div className={styles.credentialContainer}>
+          <TextField
+            className={styles.credentialTextFields}
+            onChange={(e) => handleChange(e.target.value, "username")}
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+          />
+          <TextField
+            type="password"
+            value={user.password}
+            className={styles.credentialTextFields}
+            onChange={(e) => handleChange(e.target.value, "password")}
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+          />
         </div>
-    )
-}
+        {/* <Link style={{ width:"100%", marginTop:10,height:"3rem", font}} to={{pathname:'/details'}}> */}
+        <Button onClick={handleClick} className={styles.button} variant="contained" color="primary">
+          Submit
+        </Button>
+        {/* </Link> */}
+      </div>
+    </div>
+  );
+};
